5971e0c0297816c6d814070f894245f731f27f1b
wwt2008 <wwt2008@gmail.com>
2024-12-05 10:16:34 +0300

    Remove tracked files from src in feature/analysis branch

--- src/frontend/strm/app/inner_pages/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ("login_page", "job_analyzer_page", "home_page", "logout_page")
-from .login import login_page
-from .job_analyzer import job_analyzer_page
-from .home import home_page
-from .logout import logout_page
