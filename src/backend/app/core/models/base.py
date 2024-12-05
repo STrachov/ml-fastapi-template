d5a6af1c3d6d44068ca95ceb08dcb6fefe535558
wwt2008 <wwt2008@gmail.com>
2024-12-05 16:55:32 +0300

    commit scrapping cleanings

--- src/backend/app/core/models/base.py
+++
@@ -1,8 +0,0 @@
-from pydantic import ConfigDict
-from sqlmodel import SQLModel, Field
-
-class Base(SQLModel):
-    __abstract__ = True  # Ensures SQLModel doesn't create a table for this base class
-
-    # Apply the metadata convention for SQLModel if required
-    metadata = SQLModel.metadata
